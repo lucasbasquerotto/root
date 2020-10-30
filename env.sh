2a28429e503a98834c0f154f11cd1a373bffe346
lucas99freelas <lucas99freelas@99freelas.com>
2020-10-30 09:16:05 -0300

    [dev] setup - env_main_dir

--- env.sh
+++ env.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export ctl_repo=https://github.com/lucasbasquerotto/ctl.git
 export env_repo=https://lucasbasquerotto@bitbucket.org/lucasbasquerotto/ansible-main-env-demo.git
+export env_main_link_target=repos/env-main
